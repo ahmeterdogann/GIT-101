9171db19f80bd07f2a91ad8e08157a54560022fa
ahmeterdogann <ahmett.erdogan.23@gmail.com>
2023-07-22 19:21:30 +0300

    dev commit-1

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("commit-4");
         System.out.println("commit-5");
+        System.out.println("dev commit-1");
     }
 }
